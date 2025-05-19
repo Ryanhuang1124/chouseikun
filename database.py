564d361655027ee32493cc6bf70d8e60634e8bf6
Ryanhuang1124 <ryanhuang1124@gmail.com>
2025-05-19 15:34:09 +0900

    update local dotenv

--- database.py
+++ database.py
@@ -1,10 +1,17 @@
+import os
+from dotenv import load_dotenv
 from typing import Annotated
 from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,Session
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.loylnhbshqtecebkneaj:dJkMdfB7RWKuqR8D@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres'
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("DATABASE_URL Not Set")
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 local_session = sessionmaker(autocommit=False, autoflush=False,bind = engine)
