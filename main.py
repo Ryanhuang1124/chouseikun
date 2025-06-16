21c843c0d26c5c7c61b454b23b63cfdcf5c9b288
Ryanhuang1124 <ryanhuang1124@gmail.com>
2025-06-16 12:17:17 +0900

    local host domain updates

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ app = FastAPI()
 # 設定允許的來源
 origins = [
     "http://localhost",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
     "https://14bd-202-208-112-184.ngrok-free.app", 
     "https://*.ngrok.io", 
     "https://*.ngrok-free.app" 
